c58c1fbb2b6e5e71b77ff37f6d1f5e87ba8812a0
Alex M <almc8368@colorado.edu>
2022-11-13 22:40:56 -0700

    Page version 1

--- pages/alexmcdonald.js
+++ pages/alexmcdonald.js
@@ -6,20 +6,26 @@ export default function almc() {
 
         <main>
           <div className='underline'>
-            <h1 className='title'>Hello, Im Ron Burgundy?</h1>
+            <h1 className='title'>How Asymmetric Encryption Creates Online Security</h1>
           </div>
           <img
             className='pic'
-            src='https://pbs.twimg.com/profile_images/415961110635110400/xPO9YqTC.jpeg'
-            alt='ron burgundy'
+            src='https://i.imgur.com/BquxT68.png'
+            alt='crypto key'
           />
-
+          <span>Image Credit: Wikimedia Commons</span>
           <p className='description'>
-            I dont know if you know this, but... I am kind of a big deal. I have
-            many leather bound books and my apartment smells of rich mahogany. I
-            am also a five-time local Emmy award-winning anchor for the KVWN Channel
-            4 News in San Diego. When im not reading the news, you will find me
-            playing the jazz flute around the city at different establishments.
+            <b>Basic Overview of RSA Asymmetric Encryption - Written by Alex M</b><br></br>
+            The difference between symmetric and asymmetric encryption is that symmetric encryption uses the same key to encrypt and decrypt, while asymmetric 
+            uses a private key to decrypt and distributes a mathematically related public key to encrypt. The private key is composed of two very large prime
+            numbers, and the public key is the product of those two numbers, making it an easy operation to get the public key from the private key, but a
+            nearly computationally impossible operation to calculate the private key, as the product of the two primes is hundreds of digits long. This can be
+            used to make websites secure by having the two entities (the server and the client) exchange a pair of public keys, thus allowing the client to encrypt
+            information intended for the server with the server's public key, and vice-versa with the server responding to the client. Only the server holds the private
+            key to read the information sent from the client (and there are physical devices to do this called HSMs that physically hold private keys), therefore preventing
+            a hacker from intercepting a message and being able to use it. One of the most popular algorithms for asymmetric encryption is RSA (Rivest Shamir Adleman).
+            Asymmetric encryption allows for a mathematical sender and receiver as long as the private keys are securely stored, allowing for sensitive information to
+            pass through the internet. 
           </p>
 
           <a className='button' href='/'>
@@ -65,8 +71,8 @@ export default function almc() {
           }
 
           .pic {
-            height: 450px;
-            width: 450px;
+            height: 120px;
+            width: 288px;
           }
 
           .button {
