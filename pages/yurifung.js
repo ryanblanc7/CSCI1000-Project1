27db0fb0583a69f5312bb9e36eaaa7625bb56675
Yuri <yufu7119@colorado.edu>
2022-11-19 05:09:02 +0000

    this is my third commit

--- pages/yurifung.js
+++ pages/yurifung.js
@@ -1,119 +1,120 @@
 export default function examplePage() {
-  return (
-    <div className='container'>
-      <title>Website</title>
-      <link rel='icon' href='/favicon.ico' />
+    return (
+      <div className='container'>
+        <title>Website</title>
+        <link rel='icon' href='/favicon.ico' />
 
-      <main>
-        <div className='underline'>
-          <h1 className='title'>Hello, Im Yuri Fung!</h1>
-        </div>
-        <img
-          className='pic'
-          src='https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/1200px-Flag_of_Japan.svg.png'
-          alt='Yuri Fung'
-        />
+        <main>
+          <div className='underline'>
+            <h1 className='title'>Hello, Im Yuri Fung!</h1>
+          </div>
+          <img
+             className='pic'
+             src='./watermelonGuyLol.jpg'
+             alt='ron burgundy'
+            
+          />
 
-        <p className='description'>
+          <p className='description'>
           Hello, I am Yuri Fung. Currently I am attending University of Colorado Boulder. I am majoring in Computer Science, 
-          and I plan to get a master's degree as well. I live in Erie, Colorado, and I commute to CU Boulder. I am in the class CSCI 2275, and currently learning C++.
-        </p>
+          and I plan to get a master's degree as well. I live in Erie, Colorado, and I commute to CU Boulder.
+          </p>
 
-        <a className='button' href='/'>
-          <p> Back to Home Page</p>
-        </a>
-      </main>
+          <a className='button' href='/'>
+            <p> Back to Home Page</p>
+          </a>
+        </main>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          max-width: 60rem;
-          margin: auto;
-        }
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            max-width: 60rem;
+            margin: auto;
+          }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 4rem;
+          }
 
-        .underline {
-          /* border-bottom: solid black; */
-          margin-bottom: 1.5rem;
-        }
+          .underline {
+            /* border-bottom: solid black; */
+            margin-bottom: 1.5rem;
+          }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
+          .description {
+            line-height: 1.5;
+            font-size: 1.5rem;
+          }
 
-        .pic {
-          height: 450px;
-          width: 450px;
-        }
+          .pic {
+            height: 450px;
+            width: 450px;
+          }
 
-        .button {
-          /* margin: .5rem; */
-          flex-basis: 45%;
-          padding: 0.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
+          .button {
+            /* margin: .5rem; */
+            flex-basis: 45%;
+            padding: 0.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
 
-        .button:hover,
-        .button:focus,
-        .button:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
+          .button:hover,
+          .button:focus,
+          .button:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
 
-        .button p {
-          font-size: 1rem;
-          font-weight: 600;
-        }
+          .button p {
+            font-size: 1rem;
+            font-weight: 600;
+          }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
+          code {
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          }
+        `}</style>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  );
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    );
 }
